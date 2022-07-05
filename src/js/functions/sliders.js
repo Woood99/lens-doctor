c0742b7bedb9a9e1dce5b18a81b09a6fd41acbdb
Sergey Stryuchkov <serega.stryuchkov@gmail.com>
2022-07-05 16:19:37 +0300

    finished service

--- src/js/functions/sliders.js
+++ src/js/functions/sliders.js
@@ -48,7 +48,7 @@ function initSliders() {
 
                 pagination: {
                     el: el.closest('.hero').querySelector('.hero__pagination'),
-                	clickable: true,
+                    clickable: true,
                 },
 
                 navigation: {
@@ -84,7 +84,55 @@ function initSliders() {
             });
         });
     }
+    if (document.querySelector('.service-bottom__slider')) {
+        const heroSliders = document.querySelectorAll('.service-bottom__slider').forEach(el => {
+            new Swiper(el, {
+                // autoplay: {
+                // 	delay: 3000,
+                // 	disableOnInteraction: false,
+                // },
 
+                observer: true,
+                observeParents: true,
+                slidesPerView: 1.15,
+                spaceBetween: 10, 
+                speed: 800,
+                // touchRatio: 0,
+                // simulateTouch: false,
+                // loop: true,
+                // preloadImages: false,
+                // lazy: true,
+
+
+                // pagination: {
+                //     el: el.closest('.hero').querySelector('.hero__pagination'),
+                // 	clickable: true,
+                // },
+
+                // navigation: {
+                //     nextEl: el.closest('.hero').querySelector('.hero-nav-next'),
+                //     prevEl: el.closest('.hero').querySelector('.hero-nav-prev'),
+                // },
+
+
+                breakpoints: {
+                    576: {
+                        slidesPerView: 2,
+                        spaceBetween: 15, 
+                    },
+                    1024:{
+                        slidesPerView: 3,
+                        spaceBetween: 30,
+                    }
+                },
+
+
+                on: {
+
+                }
+            });
+        });
+    }
 
 }
 
